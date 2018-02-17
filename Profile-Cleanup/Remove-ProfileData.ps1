a0053650efd2a774bff23061dd53a0f898528814
Aaron Parker <aaron@stealthpuppy.com>
2018-02-17 21:51:04 +1100

    Alpha 2

--- Profile-Cleanup/Remove-ProfileData.ps1
+++ Profile-Cleanup/Remove-ProfileData.ps1
@@ -33,6 +33,23 @@ Param (
     [string[]]$Xml
 )
 Begin {
+
+    Function ConvertTo-Path {
+        <#
+          .SYNOPSIS
+            Replaces environment variables in strings with actual path
+        #>
+        Param (
+            [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+            [string]$Path
+        )
+        Switch ($Path) {
+            { $_ -match "%LocalAppData%" } { $Path = $Path -replace "%LocalAppData%", $env:LocalAppData }
+            { $_ -match "%AppData%" } { $Path = $Path -replace "%AppData%", $env:AppData }
+            { $_ -match "%TEMP%" } { $Path = $Path -replace "%TEMP%", $env:Temp }
+        }
+        $Path
+    }
 }
 Process {
     # Read the specifed XML document
@@ -42,18 +59,33 @@ Process {
     # Select each Target XPath
     $Targets = Select-Xml -Xml $xmlDocument -XPath "//Target"
 
+    $Output = @()
+
     # Walk through each target to delete files
     ForEach ($Target in $Targets) {
-        Write-Verbose "Processing: $($Target.Node.Name)"
+        Write-Verbose "Processing target: [$($Target.Node.Name)]"
         ForEach ($Path in $Target.Node.Path) {
-            Write-Verbose "Processing: $($Path.innerText)"
-            $DateFilter = (Get-Date).AddDays(- $Path.Days)
-            $Files = Get-ChildItem -Path $Path.innerText -Include *.* -Recurse -Force | Where-Object { $_.PSIsContainer -eq $False -and $_.LastWriteTime -le $DateFilter }
-            If ($pscmdlet.ShouldProcess("File", "Delete")) {
-                $Files | Remove-Item -Force
+            Write-Verbose "Processing folder: $(ConvertTo-Path -Path $Path.innerText)"
+
+            # Get file age from Days value in XML
+            $DateFilter = (Get-Date).AddDays( - $Path.Days)
+
+            # Get files to delete from Paths and file age
+            $Files = Get-ChildItem -Path $(ConvertTo-Path -Path $Path.innerText) -Include *.* -Recurse -Force -ErrorAction SilentlyContinue `
+                | Where-Object { $_.PSIsContainer -eq $False -and $_.LastWriteTime -le $DateFilter }
+
+            $Output += $Files
+
+            # Delete files with support for -WhatIf
+            ForEach ( $File in $Files ) {
+                If ($pscmdlet.ShouldProcess($File, "Delete")) {
+                    Remove-Item -Path $File -Force
+                }
             }
         }
     }
 }
 End {
+    # Return the files array (e.g. output for logging)
+    Write-Output $Output
 }
