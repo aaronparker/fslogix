5b33b89da9898a4a90665c3e4fa27a8eebba8904
Aaron Parker <aaron@stealthpuppy.com>
2019-10-24 17:50:18 +1100

    Get-ApplicationRegistryKey.ps1 updates

--- Rules/Get-ApplicationRegistryKey.ps1
+++ Rules/Get-ApplicationRegistryKey.ps1
@@ -1,57 +1,73 @@
 <#PSScriptInfo
-    .VERSION 1.0.5
-    .GUID 9c53a0e5-1cb3-4b35-90f0-372bc7665f4f
-    .AUTHOR Aaron Parker, @stealthpuppy
-    .COMPANYNAME stealthpuppy
-    .COPYRIGHT Aaron Parker, https://stealthpuppy.com
-    .TAGS FSLogix App-Masking
-    .LICENSEURI https://github.com/aaronparker/FSLogix/blob/master/LICENSE
-    .PROJECTURI https://github.com/aaronparker/FSLogix
-    .DESCRIPTION Returns strings from well known Registry keys that define a Windows application. Used to assist in defining an FSLogix App Masking rule set.
-    .ICONURI 
-    .EXTERNALMODULEDEPENDENCIES 
-    .REQUIREDSCRIPTS 
-    .EXTERNALSCRIPTDEPENDENCIES 
-    .RELEASENOTES
-    - 1.0.1, First version pushed to the PowerShell Gallery, June 2019
-    - 1.0.2, Fix -Key parameter in ForEach loop, Add Process block for pipeline support
-    - 1.0.3, Change parameter position & pipeline support for SearchString & Key
-    - 1.0.4, Update with additional keys
-    - 1.0.5, Update output in Try/Catch
-    .PRIVATEDATA 
-#>
-<# 
-    .SYNOPSIS
-        Returns strings from well known Registry keys that define a Windows application.
 
-    .DESCRIPTION 
-        Returns strings from well known Registry keys that define a Windows application. Used to assist in defining an FSLogix App Masking rule set.
+.VERSION 1.0.5
 
-    .PARAMETER SearchString
-        An array of strings to check for application names. Defaults to "Visio", "Project".
+.GUID 9c53a0e5-1cb3-4b35-90f0-372bc7665f4f
 
-    .PARAMETER Key
-        A single key or array of Registry keys to check child keys for application details. The script includes the keys typically needed for most applications.
+.AUTHOR Aaron Parker, @stealthpuppy
 
-    .EXAMPLE
-        To search for Registry keys specific to Adobe Reader or Acrobat:
+.COMPANYNAME stealthpuppy
 
-        C:\> .\Get-ApplicationRegistryKey.ps1 -SearchString "Adobe"
+.COPYRIGHT Aaron Parker, https://stealthpuppy.com
 
-    .EXAMPLE
-        To search for Registry keys specific to Visio and Project:
+.TAGS FSLogix App-Masking
 
-        C:\> .\Get-ApplicationRegistryKey.ps1 -SearchString "Visio", "Project"
+.LICENSEURI https://github.com/aaronparker/FSLogix/blob/master/LICENSE
 
-    .EXAMPLE
-        To search for Registry keys specific to Skype for Business:
+.PROJECTURI https://github.com/aaronparker/FSLogix
 
-        C:\> .\Get-ApplicationRegistryKey.ps1 -SearchString "Skype"
+.ICONURI
 
-    .EXAMPLE
-        To search for Registry keys specific to Visio and Project by passing strings to Get-ApplicationRegistryKey.ps1 via the pilpeline, use:
+.EXTERNALMODULEDEPENDENCIES 
 
-        C:\> "Visio", "Project" | .\Get-ApplicationRegistryKey.ps1
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+- 1.0.1, First version pushed to the PowerShell Gallery, June 2019
+- 1.0.2, Fix -Key parameter in ForEach loop, Add Process block for pipeline support
+- 1.0.3, Change parameter position & pipeline support for SearchString & Key
+- 1.0.4, Update with additional keys
+- 1.0.5, Update output in Try/Catch; Fixing PowerShellGet issues on install
+
+.PRIVATEDATA
+
+#> 
+
+<#
+
+.DESCRIPTION
+Returns strings from well known Registry keys that define a Windows application. Used to assist in defining an FSLogix App Masking rule set.
+
+.SYNOPSIS
+Returns strings from well known Registry keys that define a Windows application.
+
+.PARAMETER SearchString
+An array of strings to check for application names. Defaults to "Visio", "Project".
+
+.PARAMETER Key
+A single key or array of Registry keys to check child keys for application details. The script includes the keys typically needed for most applications.
+
+.EXAMPLE
+To search for Registry keys specific to Adobe Reader or Acrobat:
+
+C:\> .\Get-ApplicationRegistryKey.ps1 -SearchString "Adobe"
+
+.EXAMPLE
+To search for Registry keys specific to Visio and Project:
+
+C:\> .\Get-ApplicationRegistryKey.ps1 -SearchString "Visio", "Project"
+
+.EXAMPLE
+To search for Registry keys specific to Skype for Business:
+
+C:\> .\Get-ApplicationRegistryKey.ps1 -SearchString "Skype"
+
+.EXAMPLE
+To search for Registry keys specific to Visio and Project by passing strings to Get-ApplicationRegistryKey.ps1 via the pilpeline, use:
+
+C:\> "Visio", "Project" | .\Get-ApplicationRegistryKey.ps1
 #>
 [OutputType([System.Array])]
 [CmdletBinding(SupportsShouldProcess = $False, HelpUri = "https://docs.stealthpuppy.com/docs/fslogix/appkeys")]
@@ -69,6 +85,7 @@ Param (
             "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Classes", "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Clients", `
             "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\RegisteredApplications")
 )
+
 begin {
     # Get current location
     $location = Get-Location
@@ -84,10 +101,10 @@ process {
                 $result = Push-Location -Path $path -ErrorAction SilentlyContinue -PassThru
             }
             catch [System.Management.Automation.ItemNotFoundException] {
-                Write-Warning -Message "$($MyInvocation.MyCommand): Item not found when changing location to $path."
+                Write-Warning -Message "Item not found when changing location to [$path]."
             }
             catch [System.Exception] {
-                Write-Warning -Message "$($MyInvocation.MyCommand): Exception when changing location to $path."
+                Write-Warning -Message "Exception when changing location to [$path]."
             }
             # If successfully changed to the target key, get child keys and match against data in the default values
             If ($result.Length -gt 0) {
@@ -104,7 +121,8 @@ process {
         }
     }
     catch [System.Exception] {
-        Throw $_
+        Write-Warning -Message "Exception accessing registry."
+        Throw $_.Exception
     }
     finally {
         # Change back to original location
